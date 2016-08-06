d1fd601d495072ea0f97850b346e7981e540af38
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2016-08-06 11:57:54 -0700

    fixed the index.js file had it exports in two words instead of one

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-exports home = function(req, res){
+exports.home = function(req, res){
 	res.render('home',{
 		title: "Harry Potter",
 		movies: ["first","second","third"]
@@ -6,12 +6,12 @@ exports home = function(req, res){
 };
 
 // movie_show
-exports movie_show = function(req, res){
+exports.movie_show = function(req, res){
 	var part = req.params.part;
 	res.send("this is the page for episode " + part);
 };
 
 //notFound
-exports notFound = function(req,res){
+exports.notFound = function(req,res){
 	res.send("This is not the page that you are looking for");
 };
