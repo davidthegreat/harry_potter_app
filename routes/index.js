61b18c9986a1af04ee0241fd3594d7f4bba218fa
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2016-08-06 00:02:19 -0700

    moved all the routes from app.js to index.js

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+exports home = function(req, res){
+	res.render('home',{
+		title: "Harry Potter",
+		movies: ["first","second","third"]
+	});
+};
+
+// movie_show
+exports movie_show = function(req, res){
+	var part = req.params.part;
+	res.send("this is the page for episode " + part);
+};
+
+//notFound
+exports notFound = function(req,res){
+	res.send("This is not the page that you are looking for");
+};
